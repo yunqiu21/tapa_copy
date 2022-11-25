be0675d063f681e687d9c7e276765d33bc9aa81e
Yunqiu Han <yunqiu21@g.ucla.edu>
2022-11-25 09:53:04 -0800

    non-blocking

--- apps/vadd/vadd.cpp
+++ apps/vadd/vadd.cpp
@@ -9,12 +9,11 @@ void Add(tapa::istream<float_v16>& a,
          tapa::ostream<float_v16>& c, 
          uint64_t n) {
   float_v16 a_chunk, b_chunk;
-  printf("%d", a.empty());
-  printf("%d", b.empty()); 
-  printf("%d\n", c.full()); 
-  // while (!a.empty() && !b.empty() && !c.full()) {
-  for (uint64_t i = 0; i < (n + 15) / 16; ++i) {  
-    printf("%d", a.try_read(a_chunk) && b.try_read(b_chunk) && c.try_write(a_chunk + b_chunk));
+  while (!a.empty() && !b.empty() && !c.full()) {
+  // for (uint64_t i = 0; i < (n + 15) / 16; ++i) {      
+    a.try_read(a_chunk);
+    b.try_read(b_chunk);
+    c.try_write(a_chunk + b_chunk);
     // printf("%d", b.try_read(b_chunk));
     // printf("%d", c.try_write(a_chunk + b_chunk));
   }
