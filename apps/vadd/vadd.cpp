645a1e51708b43df4ce2fc69625a0a7c6d60fdbd
Yunqiu Han <yunqiu21@g.ucla.edu>
2022-12-05 15:44:34 -0800

    add pipeline to vadd

--- apps/vadd/vadd.cpp
+++ apps/vadd/vadd.cpp
@@ -10,7 +10,8 @@ void Add(tapa::istream<float_v16>& a,
          uint64_t n) {
   float_v16 a_chunk, b_chunk;
   for (uint64_t i = 0; i < (n + 15) / 16;) {
-  #pragma HLS loop_tripcount min=1 max=1024*1024  
+    #pragma HLS pipeline II=1
+    #pragma HLS loop_tripcount min=1 max=1024*1024  
     if (!a.empty() && !b.empty() && !c.full()) {
       a.try_read(a_chunk);
       b.try_read(b_chunk);
@@ -23,6 +24,8 @@ void Add(tapa::istream<float_v16>& a,
 void Mmap2Stream(tapa::mmap<const float_v16> mmap, 
                  uint64_t n,
                  tapa::ostream<float_v16>& stream) {
+  #pragma HLS pipeline II=1  
+  #pragma HLS loop_tripcount min=1 max=1024*1024 
   for (uint64_t i = 0; i < (n + 15) / 16; ++i) {
     stream << mmap[i];
   }
@@ -31,6 +34,8 @@ void Mmap2Stream(tapa::mmap<const float_v16> mmap,
 void Stream2Mmap(tapa::istream<float_v16>& stream, 
                  tapa::mmap<float_v16> mmap,
                  uint64_t n) {
+  #pragma HLS pipeline II=1  
+  #pragma HLS loop_tripcount min=1 max=1024*1024 
   for (uint64_t i = 0; i < (n + 15) / 16; ++i) {
     stream >> mmap[i];
   }
