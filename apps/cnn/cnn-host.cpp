de68d7c728fcbacdf9f576dd49957f97abe6d278
Yunqiu Han <yunqiu21@g.ucla.edu>
2022-12-05 16:10:43 -0800

    identifier

--- apps/cnn/cnn-host.cpp
+++ apps/cnn/cnn-host.cpp
@@ -15,10 +15,10 @@ using std::vector;
 using std::string;
 using float_v16 = tapa::vec_t<float, 16>;
 
-void Cnn(tapa::mmap<const float_v16> input, 
+void Cnn(tapa::mmap<const float_v16> in_img, 
          tapa::mmap<const float_v16> weight, 
          tapa::mmap<const float_v16> bias, 
-         tapa::mmap<float_v16> output, 
+         tapa::mmap<float_v16> out_img, 
          uint64_t kNum, 
          uint64_t kKernel, 
          uint64_t kInImSize,
@@ -34,21 +34,21 @@ int main(int argc, char* argv[]) {
   const uint64_t kInImSize = 8;
   const uint64_t kOutImSize = 4;
 
-  const uint64_t input_aligned_size = ((kNum*kInImSize*kInImSize + 15) / 16) * 16;
+  const uint64_t in_img_aligned_size = ((kNum*kInImSize*kInImSize + 15) / 16) * 16;
   const uint64_t weight_aligned_size = ((kNum*kNum*kKernel*kKernel + 15) / 16) * 16;
   const uint64_t bias_aligned_size = ((kNum + 15) / 16) * 16;
-  const uint64_t output_aligned_size = ((kNum*kOutImSize*kOutImSize + 15) / 16) * 16;
-  vector<float> input(input_aligned_size, 0.f);
+  const uint64_t out_img_aligned_size = ((kNum*kOutImSize*kOutImSize + 15) / 16) * 16;
+  vector<float> in_img(in_img_aligned_size, 0.f);
   vector<float> weight(weight_aligned_size, 0.f);
   vector<float> bias(bias_aligned_size, 0.f);
-  vector<float> output(output_aligned_size, 0.f);
+  vector<float> out_img(out_img_aligned_size, 0.f);
 
   int64_t kernel_time_ns = tapa::invoke(
     Cnn, FLAGS_bitstream, 
-    tapa::read_only_mmap<const float>(input).reinterpret<const float_v16>(),
+    tapa::read_only_mmap<const float>(in_img).reinterpret<const float_v16>(),
     tapa::read_only_mmap<const float>(weight).reinterpret<const float_v16>(), 
     tapa::read_only_mmap<const float>(bias).reinterpret<const float_v16>(), 
-    tapa::write_only_mmap<float>(output).reinterpret<float_v16>(), 
+    tapa::write_only_mmap<float>(out_img).reinterpret<float_v16>(), 
     kNum, 
     kKernel, 
     kInImSize,
--- apps/cnn/cnn.cpp
+++ apps/cnn/cnn.cpp
@@ -18,21 +18,21 @@ void Stream2Mmap(tapa::istream<float_v16>& stream,
   }
 }
 
-void Dummy(tapa::istream<float_v16>& input_stream, 
+void Dummy(tapa::istream<float_v16>& in_img_stream, 
            tapa::istream<float_v16>& weight_stream,
            tapa::istream<float_v16>& bias_stream,
-           tapa::ostream<float_v16>& output_stream, 
+           tapa::ostream<float_v16>& out_img_stream, 
            uint64_t kNum, 
            uint64_t kKernel, 
            uint64_t kInImSize,
            uint64_t kOutImSize) {
   float_v16 dummy;
-  const uint64_t input_aligned_size = (kNum*kInImSize*kInImSize + 15) / 16;
+  const uint64_t in_img_aligned_size = (kNum*kInImSize*kInImSize + 15) / 16;
   const uint64_t weight_aligned_size = (kNum*kNum*kKernel*kKernel + 15) / 16;
   const uint64_t bias_aligned_size = (kNum + 15) / 16;
-  const uint64_t output_aligned_size = (kNum*kOutImSize*kOutImSize + 15) / 16;
-  for (uint64_t i = 0; i < input_aligned_size; i++) {
-    input_stream.read();
+  const uint64_t out_img_aligned_size = (kNum*kOutImSize*kOutImSize + 15) / 16;
+  for (uint64_t i = 0; i < in_img_aligned_size; i++) {
+    in_img_stream.read();
   }
   for (uint64_t i = 0; i < weight_aligned_size; i++) {
     weight_stream.read();
@@ -40,31 +40,31 @@ void Dummy(tapa::istream<float_v16>& input_stream,
   for (uint64_t i = 0; i < bias_aligned_size; i++) {
     bias_stream.read();
   }
-  for (uint64_t i = 0; i < output_aligned_size; i++) {
-    output_stream << dummy;
+  for (uint64_t i = 0; i < out_img_aligned_size; i++) {
+    out_img_stream << dummy;
   }
 }
 
-void Cnn(tapa::mmap<const float_v16> input, 
+void Cnn(tapa::mmap<const float_v16> in_img, 
          tapa::mmap<const float_v16> weight, 
          tapa::mmap<const float_v16> bias, 
-         tapa::mmap<float_v16> output, 
+         tapa::mmap<float_v16> out_img, 
          uint64_t kNum, 
          uint64_t kKernel, 
          uint64_t kInImSize,
          uint64_t kOutImSize) {  
-  tapa::stream<float_v16, 2> input_stream("input");
+  tapa::stream<float_v16, 2> in_img_stream("in_img");
   tapa::stream<float_v16, 2> weight_stream("weight"); 
   tapa::stream<float_v16, 2> bias_stream("bias"); 
-  tapa::stream<float_v16, 2> output_stream("output");
-  uint64_t input_size = kNum*kInImSize*kInImSize;
+  tapa::stream<float_v16, 2> out_img_stream("out_img");
+  uint64_t in_img_size = kNum*kInImSize*kInImSize;
   uint64_t weight_size = kNum*kNum*kKernel*kKernel;
   uint64_t bias_size = kNum;
-  uint64_t output_size = kNum*kOutImSize*kOutImSize;
+  uint64_t out_img_size = kNum*kOutImSize*kOutImSize;
   tapa::task()
-      .invoke(Mmap2Stream, "Mmap2Stream", input, input_stream, input_size)       
+      .invoke(Mmap2Stream, "Mmap2Stream", in_img, in_img_stream, in_img_size)       
       .invoke(Mmap2Stream, "Mmap2Stream", weight, weight_stream, weight_size)       
       .invoke(Mmap2Stream, "Mmap2Stream", bias, bias_stream, bias_size)
-      .invoke(Dummy, "Dummy", input_stream, weight_stream, bias_stream, output_stream, kNum, kKernel, kInImSize, kOutImSize)
-      .invoke(Stream2Mmap, "Stream2Mmap", output_stream, output, output_size);
+      .invoke(Dummy, "Dummy", in_img_stream, weight_stream, bias_stream, out_img_stream, kNum, kKernel, kInImSize, kOutImSize)
+      .invoke(Stream2Mmap, "Stream2Mmap", out_img_stream, out_img, out_img_size);
 }
