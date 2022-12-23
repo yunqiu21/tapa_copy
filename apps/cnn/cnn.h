3d280fc3eb745932336f591512ebbee6aa57e3ec
Yunqiu Han <yunqiu21@g.ucla.edu>
2022-12-23 15:51:50 -0800

    header

---
+++ apps/cnn/cnn.h
@@ -0,0 +1,8 @@
+#define kNum 16
+#define kInImSize 8
+#define kOutImSize 4
+#define kKernel 5
+#define in_img_size kNum*kInImSize*kInImSize
+#define out_img_size kNum*kOutImSize*kOutImSize
+#define weight_size kNum*kNum*kKernel*kKernel
+#define bias_size kNum
