4f86746322c7fb696e523c4ea4cabf04c3bdc496
Yunqiu Han <yunqiu21@g.ucla.edu>
2022-12-28 18:29:01 -0800

    array partition

--- apps/cnn/cnn.h
+++ apps/cnn/cnn.h
@@ -1,5 +1,5 @@
-#define kNum 256
-#define kOutImDim 56
+#define kNum 16
+#define kOutImDim 4
 #define kImDim (kOutImDim*2)
 #define kKernel 5
 #define kInImDim (kImDim+kKernel-1)
@@ -7,17 +7,6 @@
 #define kWeightSize (kNum*kNum*kKernel*kKernel)
 #define kBiasSize (kNum)
 #define kOutImgSize (kNum*kOutImDim*kOutImDim)
-
-// #define Input(x,y,z)    \
-//     (in_img_vec[(x)*kInImDim*kInImDim+(y)*kInImDim+(z)])
-// #define Weight(x,y,z,i) \
-//     (weight_vec[(x)*kNum*kKernel*kKernel+(y)*kKernel*kKernel+(z)*kKernel+(i)])
-// #define Bias(x)         \
-//     (bias_vec[(x)])
-// #define Output(x,y,z)   \
-//     (out_img_vec[(x)*kOutImDim*kOutImDim+(y)*kOutImDim+(z)])
-// #define C(x,y)   \
-//     (c_vec[(x)*kImDim+(y)])
 
 using float_v16 = tapa::vec_t<float, 16>;
 
